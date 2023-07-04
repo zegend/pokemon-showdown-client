cc59e9ab8b0e59218a0685ea70e5d82fd8800d10
zegend <zegendoflelda@gmail.com>
2023-07-04 18:45:08 +0100

    replay save reminder

--- js/client-battle.js
+++ js/client-battle.js
@@ -211,6 +211,7 @@
 				} else if (logLine.substr(0, 5) === '|win|' || logLine === '|tie') {
 					this.battleEnded = true;
 					this.battle.stepQueue.push(logLine);
+					this.battle.stepQueue.push('Remember to save replays!');
 				} else if (logLine.substr(0, 6) === '|chat|' || logLine.substr(0, 3) === '|c|' || logLine.substr(0, 4) === '|c:|' || logLine.substr(0, 9) === '|chatmsg|' || logLine.substr(0, 10) === '|inactive|') {
 					this.battle.instantAdd(logLine);
 				} else {
